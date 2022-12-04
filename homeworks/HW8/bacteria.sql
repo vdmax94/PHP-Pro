d89f50bf7e9fccd148c877ed24062b52451cb089
vdmax94 <vdmax94@gmail.com>
2022-12-04 12:01:26 +0200

    Homework-8

--- homeworks/HW8/bacteria.sql
+++ homeworks/HW8/bacteria.sql
@@ -7,48 +7,61 @@ CREATE TABLE divisio (
 
 CREATE TABLE classes (
                          id INT PRIMARY KEY AUTO_INCREMENT,
-                         divisio_id INT,
+                         divisio_id INT NOT NULL,
                          name VARCHAR(150),
-
-                         FOREIGN KEY (divisio_id) REFERENCES divisio(id) ON DELETE SET NULL
+                         FOREIGN KEY (divisio_id) REFERENCES divisio(id) ON DELETE NO ACTION
 );
 
 CREATE TABLE ordines (
                          id INT PRIMARY KEY AUTO_INCREMENT,
-                         class_id INT,
+                         divisio_id INT NOT NULL,
+                         class_id INT DEFAULT NULL,
                          name VARCHAR(150),
 
+                         FOREIGN KEY (divisio_id) REFERENCES divisio(id) ON DELETE NO ACTION,
                          FOREIGN KEY (class_id) REFERENCES classes(id) ON DELETE SET NULL
 );
 
 CREATE TABLE familiae (
                           id INT PRIMARY KEY AUTO_INCREMENT,
-                          ordo_id INT,
+                          divisio_id INT NOT NULL,
+                          class_id INT DEFAULT NULL,
+                          ordo_id INT DEFAULT NULL,
                           name VARCHAR(150),
 
+                          FOREIGN KEY (divisio_id) REFERENCES divisio(id) ON DELETE NO ACTION,
+                          FOREIGN KEY (class_id) REFERENCES classes(id) ON DELETE SET NULL,
                           FOREIGN KEY (ordo_id) REFERENCES ordines(id) ON DELETE SET NULL
 );
 
 CREATE TABLE genera (
                         id INT PRIMARY KEY AUTO_INCREMENT,
-                        familia_id INT,
+                        divisio_id INT NOT NULL,
+                        class_id INT DEFAULT NULL,
+                        ordo_id INT DEFAULT NULL,
+                        familia_id INT DEFAULT NULL,
                         name VARCHAR(150),
 
+                        FOREIGN KEY (divisio_id) REFERENCES divisio(id) ON DELETE NO ACTION,
+                        FOREIGN KEY (class_id) REFERENCES classes(id) ON DELETE SET NULL,
+                        FOREIGN KEY (ordo_id) REFERENCES ordines(id) ON DELETE SET NULL,
                         FOREIGN KEY (familia_id) REFERENCES familiae(id) ON DELETE SET NULL
 );
 
 CREATE TABLE species (
                          id INT PRIMARY KEY AUTO_INCREMENT,
-                         divisio_id INT,
-                         class_id INT,
-                         ordo_id INT,
-                         familia_id INT,
-                         genus_id INT,
+                         divisio_id INT NOT NULL,
+                         class_id INT DEFAULT NULL,
+                         ordo_id INT DEFAULT NULL,
+                         familia_id INT DEFAULT NULL,
+                         genus_id INT NOT NULL,
                          name VARCHAR(150),
+                         scientist VARCHAR(255) NOT NULL,
+                         year YEAR NOT NULL,
 
-                         FOREIGN KEY (divisio_id) REFERENCES divisio(id) ON DELETE SET NULL,
+                         FOREIGN KEY (divisio_id) REFERENCES divisio(id) ON DELETE NO ACTION,
                          FOREIGN KEY (class_id) REFERENCES classes(id) ON DELETE SET NULL,
                          FOREIGN KEY (ordo_id) REFERENCES ordines(id) ON DELETE SET NULL,
                          FOREIGN KEY (familia_id) REFERENCES familiae(id) ON DELETE SET NULL,
-                         FOREIGN KEY (genus_id) REFERENCES genera(id) ON DELETE SET NULL
+                         FOREIGN KEY (genus_id) REFERENCES genera(id) ON DELETE NO ACTION
 );
