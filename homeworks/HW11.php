290c034ee214dd81eb4c8722b4af1415accae347
vdmax94 <vdmax94@gmail.com>
2022-12-10 21:50:43 +0200

    Homework-11

--- homeworks/HW11.php
+++ homeworks/HW11.php
@@ -1,1 +1,42 @@
 <?php
+/*
+interface Bird
+{
+    public function eat();
+    public function fly();
+}
+
+class Swallow implements Bird
+{
+    public function eat() { ... }
+    public function fly() { ... }
+}
+
+class Ostrich implements Bird
+{
+    public function eat() { ... }
+    public function fly() { /* exception */ /*}
+} */
+
+# Розділяємо інтерфейс Bird на два зі своїми специфічними методами
+interface ISwallow
+{
+    public function eat();
+    public function fly();
+}
+
+interface IOstrich
+{
+    public function eat();
+}
+
+#А потім відповідний клас імплементує відповідний інтерфейс
+class Swallow implements ISwallow {
+    public function eat(){}
+    public function fly(){}
+}
+
+class Ostrich implements IOstrich {
+    public function eat(){}
+}
+
--- homeworks/HW8/bacteria.sql
+++ homeworks/HW8/bacteria.sql
@@ -57,11 +57,10 @@ CREATE TABLE species (
                          genus_id INT NOT NULL,
                          name VARCHAR(150) NOT NULL,
                          scientist VARCHAR(255) NOT NULL,
-                         year YEAR NOT NULL,
+                         year SMALLINT NOT NULL,
 
                          FOREIGN KEY (divisio_id) REFERENCES divisio(id) ON DELETE NO ACTION,
                          FOREIGN KEY (class_id) REFERENCES classes(id) ON DELETE SET NULL,
                          FOREIGN KEY (ordo_id) REFERENCES ordines(id) ON DELETE SET NULL,
                          FOREIGN KEY (familia_id) REFERENCES familiae(id) ON DELETE SET NULL,
-                         FOREIGN KEY (genus_id) REFERENCES genera(id) ON DELETE NO ACTION
-);
+                         FOREIGN KEY (genus_id) REFERENCES genera(id) ON DELETE NO ACTION);
